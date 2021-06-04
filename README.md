b656f8ff4722eb61c297203ae3460d9239a5aa21
geecrypt <gavina352@gmail.com>
2021-06-03 21:40:45 -0700

    create minimal flask app

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# jobscraper
+# Jobscraper
 
 Scrape job sites for keywords
 
 - only supports Indeed.com
 - Stores results in csv file
 - Fully traverses set of results pages
+
+***
+
+## Setup
+
+Activate venv (`python3`) and install modules
+
+```shell
+$ python3 -m venv env 
+$ source env/bin/activate
+$ pip install -r requirements.txt
+```
+
+Check that flask is set up
+
+```shell
+$ python3 -c "import flask; print(flask.__version__)"
+```
+
+## Run
+
--- scraper.py
+++ scraper.py
@@ -1,6 +1,3 @@
-#pylint: disable-all
-#Indeed job scraper
-
 import csv
 import sys
 from datetime import datetime
@@ -8,19 +5,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
-def get_url(term1, term2):
-    """Generate a url from 2 search terms"""
-    template = 'https://ca.indeed.com/jobs?q={}+{}&sort=relevance'    
-    url = template.format(term1, term2)
+def get_url(terms):
+    """Generate a url from search terms"""
+    url = 'https://ca.indeed.com/jobs?q=%s&sort=relevance' % '+'.join(
+        [str(t) for t in terms])
     return url
+
 
 def get_record(card):
     """Extract job data from a single record"""
     atag = card.h2.a
     job_title = atag.get('title')
     print('JOB TITLE:' + job_title)
-    atag = card.h2.a
     job_url = 'https://ca.indeed.com' + atag.get('href')
     response = requests.get(job_url)
     card_soup = BeautifulSoup(response.text, 'html.parser')
@@ -30,6 +26,7 @@ def get_record(card):
         print('No job description')
         raise AttributeError
 
+    # Only return a record if it contains the word "certification"
     text_lines = text.split('\n')
     for line in text_lines:
         if 'certification' in line:
@@ -37,17 +34,20 @@ def get_record(card):
             print(line)
             return record
 
-def main(term1, term2):
+
+def scrape(*terms):
     """Main routine"""
-    url = get_url(term1, term2)
+    print(list(terms))
+    url = get_url(terms)
+    print(url)
     records = []
 
     # Check all pages
-    while True:
+    while len(records) < 5:
         response = requests.get(url)
         page_soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Check all cards on page
+        # Check all job cards on page
         cards = page_soup.find_all('div', 'jobsearch-SerpJobCard')
         for card in cards:
             try:
@@ -55,14 +55,18 @@ def main(term1, term2):
             except AttributeError:
                 continue
             if record is not None:
-                records.append(record)            
+                records.append(record)
+                if len(records) == 10:
+                    break
 
+        # Get URL for next page of results
         try:
-            url = 'https://ca.indeed.com' + page_soup.find('a', {'aria-label': 'Next'}).get('href')
+            url = 'https://ca.indeed.com' + \
+                page_soup.find('a', {'aria-label': 'Next'}).get('href')
         except AttributeError:
             print("End of results")
             break
-    
+
     for record in records:
         print(record)
 
@@ -71,5 +75,11 @@ def main(term1, term2):
         writer.writerow(['Job Title', 'URL', 'Contents'])
         writer.writerows(records)
 
-#Run main routine
-main('junior', 'devops')
+    return records
+
+
+# def main():
+#     scrape("cloud", "security", "junior")
+
+
+# main()
